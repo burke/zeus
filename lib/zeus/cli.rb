116044783a16b42e757a94e4d8fa995ae2291971
Stefan Penner <stefan.penner@gmail.com>
2012-07-27 17:36:10 -0400

    - extract CLI behaviour out into the CLI class - some useful error messages around common potential mistakes - continue to ensure sockets are cleaned up

--- lib/zeus/cli.rb
+++ lib/zeus/cli.rb
@@ -14,6 +14,57 @@ Commands:
 BANNER
 
   def banner
-    BANNER
+    puts BANNER
+  end
+
+  def not_running(error)
+    abort <<-ABORT
+[#{error}]
+Zeus may not running, try: 'zeus start'
+
+#{BANNER}
+    ABORT
+  end
+
+  def no_dot_zues(error)
+    abort <<-ABORT
+[#{error}]
+Not .zues.rb found
+
+#{BANNER}
+ABORT
+  end
+
+  def start
+    command = ARGV[0]
+
+    if command == "start"
+      dot_zeus
+      server
+    elsif command =~ /h|help/i or ARGV.empty?
+      banner
+    else
+      client
+    end
+  end
+
+  def dot_zeus
+    require './.zeus.rb'
+  rescue LoadError => e
+    no_dot_zues(e)
+    abort
+  end
+
+  def server
+    Zeus::Server.run
+  ensure
+    Zeus::Client.cleanup!
+  end
+
+  def client
+    Zeus::Client.run
+  rescue Errno::ECONNREFUSED, Errno::ENOENT => e
+    not_running(e)
+    abort
   end
 end
