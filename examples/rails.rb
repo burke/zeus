a09ef2802c2bfef302d3946079268951a384e3d5
Burke Libbey <burke@libbey.me>
2012-07-26 20:41:32 -0400

    refactor

--- examples/rails.rb
+++ examples/rails.rb
@@ -1,67 +1,81 @@
 require 'socket'
 
-forkpoint boot: ->{
-  ENV_PATH  = File.expand_path('../config/environment',  __FILE__)
-  BOOT_PATH = File.expand_path('../config/boot',  __FILE__)
-  APP_PATH  = File.expand_path('../config/application',  __FILE__)
-  ROOT_PATH = File.expand_path('..',  __FILE__)
+Zeus::Server.define! do
+  stage :boot do
 
-  require BOOT_PATH
+    action do
+      ENV_PATH  = File.expand_path('../config/environment',  __FILE__)
+      BOOT_PATH = File.expand_path('../config/boot',  __FILE__)
+      APP_PATH  = File.expand_path('../config/application',  __FILE__)
+      ROOT_PATH = File.expand_path('..',  __FILE__)
 
-  forkpoint rails: ->{
-    require 'rails/all'
+      require BOOT_PATH
+    end
 
-    forkpoint default_bundle: -> {
-      Bundler.require(:default)
+    stage :rails do
+      action { require 'rails/all' }
 
-      forkpoint(
-        test: ->{
-          ENV['RAILS_ENV'] = "test"
-          Bundler.require(:test)
-          require APP_PATH
-          Rails.application.require_environment!
+      stage :default_bundle do
+        action { Bundler.require(:default) }
 
-          forkpoint testrb: acceptor(".zeus.test_testrb.sock") {
-            (r = Test::Unit::AutoRunner.new(true)).process_args(ARGV) or
-              abort r.options.banner + " tests..."
-            exit r.run
-          }
-        },
+        stage :dev do
+          action do
+            Bundler.require(:development)
+            ENV['RAILS_ENV'] = "development"
+            require APP_PATH
+            Rails.application.require_environment!
+          end
 
-        dev: ->{
-          Bundler.require(:development)
-          ENV['RAILS_ENV'] = "development"
-          require APP_PATH
-          Rails.application.require_environment!
+          acceptor :generate, ".zeus.dev_generate.sock" do
+            require 'rails/commands/generate'
+          end
+          acceptor :runner, ".zeus.dev_runner.sock" do
+            require 'rails/commands/runner'
+          end
+          acceptor :console, ".zeus.dev_console.sock" do
+            require 'rails/commands/console'
+            Rails::Console.start(Rails.application)
+          end
 
-          forkpoint(
-            generate: acceptor(".zeus.dev_generate.sock") {
-              require 'rails/commands/generate'
-            },
-            console: acceptor(".zeus.dev_console.sock") {
-              require 'rails/commands/console'
-              Rails::Console.start(Rails.application)
-            },
-            runnner: acceptor(".zeus.dev_runner.sock") {
-              require 'rails/commands/runner'
-            },
-            server: acceptor(".zeus.dev_server.sock") {
-              require 'rails/commands/server'
-              server = Rails::Server.new
-              Dir.chdir(Rails.application.root)
-              server.start
-            },
-            rake: ->{
+          acceptor :server, ".zeus.dev_server.sock" do
+            require 'rails/commands/server'
+            server = Rails::Server.new
+            Dir.chdir(Rails.application.root)
+            server.start
+          end
+
+          stage :prerake do
+            action do
               require 'rake'
               load 'Rakefile'
+            end
 
-              forkpoint rake2: acceptor(".zeus.dev_rake.sock") {
-                Rake.application.run
-              }
+            acceptor :rake, ".zeus.dev_rake.sock" do
+              Rake.application.run
+            end
+
+          end
+        end
+
+        stage :test do
+          action do
+            ENV['RAILS_ENV'] = "test"
+            Bundler.require(:test)
+            require APP_PATH
+            Rails.application.require_environment!
+          end
+
+          acceptor :testrb, ".zeus.test_testrb.sock" do
+            forkpoint testrb: acceptor(".zeus.test_testrb.sock") {
+              (r = Test::Unit::AutoRunner.new(true)).process_args(ARGV) or
+                abort r.options.banner + " tests..."
+              exit r.run
             }
-          )
-        }
-      )
-    }
-  }
-}
+          end
+
+        end
+
+      end
+    end
+  end
+end
--- lib/zeus/server.rb
+++ lib/zeus/server.rb
@@ -1,168 +1,108 @@
-require "json"
-require "socket"
+require 'json'
+require 'socket'
 
 module Zeus
-
-  class Server
-
-    def self.start!
-      config = File.read(".zeus.rb")
-      new.send(:eval,config)
+  module Server
+    def self.define!(&b)
+      @@root = Stage.new("(root)")
+      @@root.instance_eval(&b)
     end
 
-    trap("INT") { exit 0 }
+    def self.run
+      trap("INT") { exit 0 }
+      @@root_stage_pid = @@root.run
+      Process.waitall
+    end
 
-    class Watchdog
-      attr_reader :hierarchy
-      def initialize(r, w)
-        @r, @w = r, w
-        @clients = []
-        @hierarchy = ClientHierarchy.new
+    class Stage
+      def initialize(name)
+        @name = name
+        @stages, @actions = [], []
       end
 
-      class ClientHierarchy
-        def initialize
-          @tree = {}
-        end
+      def action(&b)
+        @actions << b
+      end
 
-        attr_reader :tree
+      def stage(name, &b)
+        @stages << Stage.new(name).tap { |s| s.instance_eval(&b) }
+      end
 
-        def add(client)
-          hash = find_children_hash_for_pid(client.ppid)
-          hash[client] = {}
-        end
+      def acceptor(name, socket, &b)
+        @stages << Acceptor.new(name, socket, &b)
+      end
 
-        def notify_changed(feature, root = @tree, parent = nil)
-          root.each do |k, v|
-            if k.features.include?(feature)
-              root.delete(k)
-              puts "\x1b[36m[ZEUS] killing client `#{parent ? parent.tag : "(root)"}` due to dependency change\x1b[0m"
-              Process.kill("INT", k.ppid)
-            else
-              notify_changed(feature, v, k)
-            end
-          end
-        end
+      # There are a few things we want to accomplish:
+      # 1. Running all the actions (each time this stage is killed and restarted)
+      # 2. Starting all the substages (and restarting them when necessary)
+      # 3. Starting all the acceptors (and restarting them when necessary)
+      def run
+        fork {
+          $0 = "zeus spawner: #{@name}"
+          puts "\x1b[35m[zeus] starting spawner `#{@name}`\x1b[0m"
 
-        def find_children_hash_for_pid(pid, root = @tree)
-          root.each do |k, v|
-            return v if k.pid == pid
-            if hash = find_children_hash_for_pid(pid, v)
-              return hash
-            end
+          @actions.each(&:call)
+
+          pids = {}
+          @stages.each do |stage|
+            pids[stage] = stage.run
           end
 
-          root == @tree ? @tree : nil
-        end
-      end
+          loop do
+            pid = Process.wait
+            if (status = $?.exitstatus) > 0
+              exit status
+            else # restart the stage that died.
+              stage = pids[pid]
+              pids[stage] = stage.run
+            end
+          end
 
-      class Client < Struct.new(:tag, :pid, :ppid, :features)
-
-      end
-
-      def add_client(tag, pid, ppid, features)
-        client = Client.new(tag, pid, ppid, features)
-        @hierarchy.add(client)
-        @clients << client
-      end
-
-      def register_client(message)
-        pid      = message.fetch('pid')
-        ppid     = message.fetch('ppid')
-        features = message.fetch('features')
-        tag      = message.fetch('tag')
-
-        add_client(tag, pid, ppid, features)
-
-        puts "\x1b[36m[ZEUS] started client `#{tag}` (#{features.size} features)\x1b[0m"
-      end
-
-      def run!
-        loop {
-          message = JSON.load(@r.readline.chomp)
-          register_client message
         }
       end
+
     end
 
-    r, $w = IO.pipe
-
-    watchdog_pid = fork {
-      $0 = "zeus watchdog"
-      watchdog = Watchdog.new(r, $w)
-      trap("USR1") {
-        puts "\x1b[31mPretending a file in mocha changed... fsevents soon\x1b[0m"
-        watchdog.hierarchy.notify_changed("/Users/burke/.rbenv/versions/1.9.3-p125-perf/lib/ruby/gems/1.9.1/gems/oauth-0.4.4/lib/oauth/request_proxy.rb")
-      }
-      watchdog.run!
-    }
-
-    def register_dependency(tag, pid, ppid, features)
-      msg = {tag: tag, pid: pid, ppid: ppid, features: features}.to_json
-      $w.puts msg
-    end
-
-    def forkpoint(forks)
-      pids = {}
-      forks.each do |k, v|
-        pid = fork {
-          $0 = "zeus spawner: #{k}"
-          v.call
-          loop { sleep 100 }
-      }
-      register_dependency(k, pid, Process.pid, $LOADED_FEATURES)
-      pids[pid] = k
-    end
-    loop {
-      pid = Process.wait
-      if (status = $?.exitstatus) > 0
-        exit status
-      else # restart
-        name = pids[pid]
-        pids.delete(pid)
-        new_pid = fork(&forks[name])
-        register_dependency(name, new_pid, Process.pid, $LOADED_FEATURES)
-        pids[new_pid] = name
+    class Acceptor
+      def initialize(name, socket, &b)
+        @name = name
+        @socket = socket
+        @action = b
       end
-    }
+
+      def run
+        fork {
+          $0 = "zeus acceptor: #{@name}"
+          puts "\x1b[35m[zeus] starting acceptor `#{@name}`\x1b[0m"
+
+          File.unlink(@socket) rescue nil
+          server = UNIXServer.new(@socket)
+          loop do
+            ActiveRecord::Base.clear_all_connections! # TODO : refactor
+            client = server.accept
+            child = fork do
+              ActiveRecord::Base.establish_connection # TODO :refactor
+              ActiveSupport::DescendantsTracker.clear
+              ActiveSupport::Dependencies.clear
+
+              terminal = client.recv_io
+              arguments = JSON.load(client.gets.strip)['arguments']
+
+              client << {status: "OK", pid: $$}.to_json << "\n"
+              $stdin.reopen(terminal)
+              $stdout.reopen(terminal)
+              $stderr.reopen(terminal)
+              ARGV.replace(arguments)
+
+              @action.call
+            end
+            Process.detach(child)
+            client.close
+          end
+        }
+      end
+
+    end
+
   end
-
-  def acceptor(socket, &b)
-    ->{
-      $0 = $0.sub(/spawner/, "acceptor")
-      File.unlink(socket) rescue nil
-      server = UNIXServer.new(socket)
-      loop {
-        client = server.accept
-        ActiveRecord::Base.clear_all_connections!
-        child = fork do
-          ActiveRecord::Base.establish_connection
-          ActiveSupport::DescendantsTracker.clear
-          ActiveSupport::Dependencies.clear
-
-          terminal = client.recv_io
-          args = JSON.load(client.gets.strip).values_at("arguments", "environment")
-          arguments, environment = args
-
-
-          # if command == "test" and environment == "test"
-          client << { status: "OK", pid: $$ }.to_json << "\n"
-          # else
-          #   client << { status: "NO", message: "invalid arguments or environment" }.to_json << "\n"
-          #   exit
-          # end
-
-          $stdin.reopen(terminal)
-          $stdout.reopen(terminal)
-          $stderr.reopen(terminal)
-          ARGV.replace(arguments)
-
-          b.call
-        end
-        Process.detach(child)
-        client.close
-      }
-    }
-  end
-end
 end
