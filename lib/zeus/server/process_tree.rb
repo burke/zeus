1c02670358956a164f69b6b6c9af15e8286a5397
Burke Libbey <burke@libbey.me>
2012-08-07 11:44:51 -0500

    Added ProcessTree specs

--- lib/zeus/server/process_tree.rb
+++ lib/zeus/server/process_tree.rb
@@ -1,10 +1,60 @@
 module Zeus
   class Server
     class ProcessTree
+
+      def initialize
+        @root = Node.new(Process.pid)
+        @nodes_by_pid = {Process.pid => @root}
+      end
+
+      def process_has_parent(pid, ppid)
+        curr = node_for_pid(pid)
+        base = node_for_pid(ppid)
+        base.add_child(curr)
+      end
+
+      def process_has_feature(pid, feature)
+        node = node_for_pid(pid)
+        node.add_feature(feature)
+      end
+
+      def kill_nodes_with_feature(file, base = @root)
+        if base.has_feature?(file)
+          kill_node(base)
+        else
+          base.children.dup.each do |node|
+            if kill_nodes_with_feature(file, node)
+              base.children.delete(node)
+            end
+          end
+          return false
+        end
+      end
+
+      private
+
+      def node_for_pid(pid)
+        @nodes_by_pid[pid.to_i] ||= Node.new(pid.to_i)
+      end
+
+      def kill_node(node)
+        if node == @root.children[0] || node == @root
+          Zeus.ui.error "One of zeus's dependencies changed. Not killing zeus. You may have to restart the server."
+          return false
+        end
+        @nodes_by_pid.delete(node.pid)
+        node.kill
+      end
+
       class Node
         attr_accessor :pid, :children, :features
         def initialize(pid)
           @pid, @children, @features = pid, [], {}
+        end
+
+        def kill
+          # recall that this process explicitly traps INT -> exit 0
+          Process.kill("INT", pid)
         end
 
         def add_child(node)
@@ -19,58 +69,6 @@ module Zeus
           self.features[feature] == true
         end
 
-        def inspect
-          "(#{pid}:#{features.size}:[#{children.map(&:inspect).join(",")}])"
-        end
-
-      end
-
-      def inspect
-        @root.inspect
-      end
-
-      def initialize
-        @root = Node.new(Process.pid)
-        @nodes_by_pid = {Process.pid => @root}
-      end
-
-      def node_for_pid(pid)
-        @nodes_by_pid[pid.to_i] ||= Node.new(pid.to_i)
-      end
-
-      def process_has_parent(pid, ppid)
-        curr = node_for_pid(pid)
-        base = node_for_pid(ppid)
-        base.add_child(curr)
-      end
-
-      def process_has_feature(pid, feature)
-        node = node_for_pid(pid)
-        node.add_feature(feature)
-      end
-
-      def kill_node(node)
-        @nodes_by_pid.delete(node.pid)
-        # recall that this process explicitly traps INT -> exit 0
-        Process.kill("INT", node.pid)
-      end
-
-      def kill_nodes_with_feature(file, base = @root)
-        if base.has_feature?(file)
-          if base == @root.children[0] || base == @root
-            Zeus.ui.error "One of zeus's dependencies changed. Not killing zeus. You may have to restart the server."
-            return false
-          end
-          kill_node(base)
-          return true
-        else
-          base.children.dup.each do |node|
-            if kill_nodes_with_feature(file, node)
-              base.children.delete(node)
-            end
-          end
-          return false
-        end
       end
 
     end
---
+++ spec/server/process_tree_spec.rb
@@ -0,0 +1,65 @@
+require 'zeus'
+
+class Zeus::Server
+
+  describe ProcessTree do
+
+    ROOT_PID = Process.pid
+    CHILD_1 = ROOT_PID + 1
+    CHILD_2 = ROOT_PID + 2
+    GRANDCHILD_1 = ROOT_PID + 3
+    GRANDCHILD_2 = ROOT_PID + 4
+
+    let(:process_tree) { ProcessTree.new }
+
+    before do
+      build_tree
+      add_features
+    end
+
+    it "doesn't kill the root node" do
+      Zeus.ui.should_receive(:error).with(/not killing zeus/i)
+      Process.should_not_receive(:kill)
+      process_tree.kill_nodes_with_feature("zeus")
+    end
+
+    it "kills a node that has a feature" do
+      expect_kill(CHILD_2)
+      process_tree.kill_nodes_with_feature("rails")
+    end
+
+    it "kills multiple nodes at the same level with a feature" do
+      expect_kill(GRANDCHILD_1)
+      expect_kill(GRANDCHILD_2)
+      process_tree.kill_nodes_with_feature("model")
+    end
+
+    private
+
+    def expect_kill(pid)
+      Process.should_receive(:kill).with("INT", pid)
+    end
+
+    def build_tree
+      process_tree.process_has_parent(CHILD_1, ROOT_PID)
+      process_tree.process_has_parent(CHILD_2, CHILD_1)
+      process_tree.process_has_parent(GRANDCHILD_1, CHILD_2)
+      process_tree.process_has_parent(GRANDCHILD_2, CHILD_2)
+    end
+
+    def add_features
+      [CHILD_2, GRANDCHILD_1, GRANDCHILD_2].each do |pid|
+        process_tree.process_has_feature(pid, "rails")
+      end
+
+      process_tree.process_has_feature(GRANDCHILD_1, "model")
+      process_tree.process_has_feature(GRANDCHILD_2, "model")
+
+      [ROOT_PID, CHILD_1, CHILD_2, GRANDCHILD_1, GRANDCHILD_2].each do |pid|
+        process_tree.process_has_feature(pid, "zeus")
+      end
+    end
+
+  end
+
+end
