a5ebc044480a8597823e6e2ebf2f0c148091ac97
Burke Libbey <burke@libbey.me>
2012-08-17 12:09:37 -0500

    Zeus::Client was a little too long. Extracted a few related methods

--- lib/zeus/client.rb
+++ lib/zeus/client.rb
@@ -11,8 +11,11 @@ require "json"
 require "pty"
 require "socket"
 
+require 'zeus/client/winsize'
+
 module Zeus
   class Client
+    include Winsize
 
     attr_accessor :pid
 
@@ -31,24 +34,25 @@ module Zeus
         PTY.open do |master, slave|
           @master = master
           set_winsize
-
-          @winch = make_winch_channel
+          make_winch_channel
           @pid = connect_to_server(command, args, slave)
 
-          buffer = ""
-          begin
-            while ready = select([@winch, @master, $stdin])[0]
-              handle_winch          if ready.include?(@winch)
-              handle_stdin(buffer)  if ready.include?($stdin)
-              handle_master(buffer) if ready.include?(@master)
-            end
-          rescue EOFError
-          end
+          select_loop!
         end
       end
     end
 
     private
+
+    def select_loop!
+      buffer = ""
+      while ready = select([winch, @master, $stdin])[0]
+        handle_winch          if ready.include?(winch)
+        handle_stdin(buffer)  if ready.include?($stdin)
+        handle_master(buffer) if ready.include?(@master)
+      end
+    rescue EOFError
+    end
 
     def connect_to_server(command, arguments, slave, socket_path = Zeus::SOCKET_NAME)
       socket = UNIXSocket.new(socket_path)
@@ -59,25 +63,8 @@ module Zeus
       pid = socket.readline.chomp.to_i
     rescue Errno::ENOENT, Errno::ECONNREFUSED, Errno::ECONNRESET
       # we need a \r at the end because the terminal is in raw mode.
-      # we need to reset the cursor to position 0
       Zeus.ui.error "Zeus doesn't seem to be running, try 'zeus start`\r"
       exit 1
-    end
-
-    def make_winch_channel
-      winch, winch_ = IO.pipe
-      trap("WINCH") { winch_ << "\0" }
-      winch
-    end
-
-    def handle_winch
-      @winch.read(1)
-      set_winsize
-      begin
-        Process.kill("WINCH", pid) if pid
-      rescue Errno::ESRCH
-        exit # the remote process died. Just quit.
-      end
     end
 
     def handle_stdin(buffer)
@@ -96,10 +83,6 @@ module Zeus
       $stdout << @master.readpartial(4096, buffer)
     end
 
-    def set_winsize
-      $stdout.tty? and @master.winsize = $stdout.winsize
-    end
-
     def maybe_raw(&b)
       if $stdout.tty?
         $stdout.raw(&b)
@@ -110,5 +93,3 @@ module Zeus
 
   end
 end
-
-__FILE__ == $0 and Zeus::Client.run
