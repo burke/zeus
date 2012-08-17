27d178716bd6aa39656300fab8547702f20f8d87
Burke Libbey <burke@libbey.me>
2012-08-17 12:26:25 -0500

    Extract some hefty inline documentation into docs/ directory

--- lib/zeus/server/client_handler.rb
+++ lib/zeus/server/client_handler.rb
@@ -1,28 +1,9 @@
 require 'socket'
 require 'json'
 
+# This class is a little confusing. See the docs/ directory for guidance.
 module Zeus
   class Server
-    # The model here is kind of convoluted, so here's an explanation of what's
-    # happening with all these sockets:
-    #
-    # #### Acceptor Registration
-    # 1. ClientHandler creates a socketpair for Acceptor registration (S_REG)
-    # 2. When an acceptor is spawned, it:
-    #   1. Creates a new socketpair for communication with clienthandler (S_ACC)
-    #   2. Sends one side of S_ACC over S_REG to clienthandler.
-    #   3. Sends a JSON-encoded hash of `pid`, `commands`, and `description`. over S_REG.
-    # 3. ClientHandler received first the IO and then the JSON hash, and stores them for later reference.
-    #
-    # #### Running a command
-    # 1. ClientHandler has a UNIXServer (SVR) listening.
-    # 2. ClientHandler has a socketpair with the acceptor referenced by the command (see Registration) (S_ACC)
-    # 3. When clienthandler received a connection (S_CLI) on SVR:
-    #   1. ClientHandler sends S_CLI over S_ACC, so the acceptor can communicate with the server's client.
-    #   2. ClientHandler sends a JSON-encoded array of `arguments` over S_ACC
-    #   3. Acceptor sends the newly-forked worker PID over S_ACC to clienthandler.
-    #   4. ClientHandler forwards the pid to the client over S_CLI.
-    #
     class ClientHandler
       def datasource          ; @listener ; end
       def on_datasource_event ; handle_server_connection ; end
@@ -43,20 +24,14 @@ module Zeus
 
       private
 
-      #  client clienthandler acceptor
-      # 1  ---------->                | {command: String, arguments: [String]}
-      # 2  ---------->                | Terminal IO
-      # 3            ----------->     | Terminal IO
-      # 4            ----------->     | Arguments (json array)
-      # 5            <-----------     | pid
-      # 6  <---------                 | pid
+      # See docs/client_server_handshake.md for details
       def handle_server_connection
         s_client = @listener.accept
 
-        data = JSON.parse(s_client.readline.chomp) # step 1
+        data = JSON.parse(s_client.readline.chomp)
         command, arguments = data.values_at('command', 'arguments')
 
-        client_terminal = s_client.recv_io # step 2
+        client_terminal = s_client.recv_io
 
         Thread.new {
           # This is a little ugly. Gist: Try to handshake the client to the acceptor.
@@ -81,16 +56,16 @@ module Zeus
           return exit_with_message(s_client, client_terminal, msg)
         end
 
-        unless acceptor = send_io_to_acceptor(client_terminal, command) # step 3
+        unless acceptor = send_io_to_acceptor(client_terminal, command)
           wait_for_acceptor(s_client, client_terminal, command)
           exit REATTEMPT_HANDSHAKE
         end
 
         Zeus.ui.info "accepting connection for #{command}"
 
-        acceptor.socket.puts arguments.to_json # step 4
-        pid = acceptor.socket.readline.chomp.to_i # step 5
-        s_client.puts pid # step 6
+        acceptor.socket.puts arguments.to_json
+        pid = acceptor.socket.readline.chomp.to_i
+        s_client.puts pid
         s_client.close
       end
 
@@ -122,7 +97,6 @@ module Zeus
       rescue Errno::EPIPE
         return false
       end
-
 
     end
   end
