4c9ef9b088a7867799e5affc90c8e374d4993379
Burke Libbey <burke@libbey.me>
2012-08-28 17:05:16 -0500

    Removed more unused stuff I forgot on the last commit

--- zeusclient/zeusclient.go
+++ zeusclient/zeusclient.go
@@ -14,36 +14,8 @@ import (
 )
 
 const (
-	sys_TIOCGETA = 0x40487413
-	sys_TIOCSETA = 0x80487414
-	sys_ISTRIP = 0x20
-	sys_INLCR = 0x40
-	sys_ICRNL = 0x100
-	sys_IGNCR = 0x80
-	sys_IXON = 0x200
-	sys_IXOFF = 0x400
-	sys_ECHO = 0x8
-	sys_ICANON = 0x100
-	sys_ISIG = 0x80
-	termios_NCCS = 20
-
 	zeusSockName = ".zeus.sock"
 )
-
-type tcflag_t uint64 // unsigned long
-type cc_t byte       // unsigned char
-type speed_t uint64  // unsigned long
-
-type struct_termios struct {
-	c_iflag tcflag_t       /* input flags */
-	c_oflag tcflag_t       /* output flags */
-	c_cflag tcflag_t       /* control flags */
-	c_lflag tcflag_t       /* local flags */
-	c_cc[termios_NCCS] cc_t /* control chars */
-	c_ispeed speed_t      /* input speed */
-	c_ospeed speed_t      /* output speed */
-}
-
 
 func Run() {
 	master, slave, err := pty.Open()
