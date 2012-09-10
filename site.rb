69d0a06f56daa13752e02759b1817e68d5dd52e2
Burke Libbey <burke@libbey.me>
2012-09-10 16:28:10 -0500

    stufF

---
+++ site.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+
+get '/?' do
+  redirect '/here' 
+end
+
+get '/here/?' do
+  erb :here
+end
+
+get '/useful/?' do
+  erb :useful
+end
+
+get '/easy/?' do
+  erb :easy
+end
+
+get '/frequently/questionable/?' do
+  erb :faq
+end
+
+get '/open/source/?' do
+  erb :contributing
+end
+
+get '/configurable/?' do
+  erb :configuring
+end
+
+get '/documented?' do
+  erb :documentation
+end
