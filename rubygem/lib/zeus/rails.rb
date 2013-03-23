085937acc14f6222477703f8963ab2f7427c4b30
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-03-23 19:55:23 +0100

    Allow to pass arguments to test method
    This will let to pass arguments to i.e. Rspec from within other plan tasks.
    I need this to finish up my integration of Zeus and Parallel Tests

--- rubygem/lib/zeus/rails.rb
+++ rubygem/lib/zeus/rails.rb
@@ -181,13 +181,13 @@ module Zeus
       end
     end
 
-    def test
-      if spec_file?(ARGV) && defined?(RSpec)
+    def test(argv=ARGV)
+      if spec_file?(argv) && defined?(RSpec)
         # disable autorun in case the user left it in spec_helper.rb
         RSpec::Core::Runner.disable_autorun!
-        exit RSpec::Core::Runner.run(ARGV)
+        exit RSpec::Core::Runner.run(argv)
       else
-        Zeus::M.run(ARGV)
+        Zeus::M.run(argv)
       end
     end
 
