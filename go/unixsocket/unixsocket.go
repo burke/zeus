dfe348db369ba34c0df716186d2de86fed65d226
Philipp Fehre <philipp.fehre@googlemail.com>
2013-05-08 10:01:58 +0200

    menomize sockName
    so we don't need to get it more than once from the env

--- go/unixsocket/unixsocket.go
+++ go/unixsocket/unixsocket.go
@@ -14,6 +14,8 @@ import (
 // http://code.google.com/p/rsc/source/browse/fuse/mount_linux.go
 // https://github.com/hanwen/go-fuse/blob/master/fuse/mount.go
 // http://code.google.com/p/go/source/browse/src/pkg/syscall/syscall_bsd.go?spec=svn982df2b2cb4b6001e8b60f9e8a000751e9a42198&name=982df2b2cb4b&r=982df2b2cb4b6001e8b60f9e8a000751e9a42198
+var sockName string
+var sockSet bool
 
 type Usock struct {
 	Conn           *net.UnixConn
@@ -24,10 +26,15 @@ type Usock struct {
 }
 
 func ZeusSockName() string {
-  var sockName = os.Getenv("ZEUSSOCK")
+  if sockSet {
+    return sockName
+  }
+
+  sockName = os.Getenv("ZEUSSOCK")
   if sockName == "" {
     sockName = ".zeus.sock"
   }
+  sockSet = true
   return sockName
 }
 
