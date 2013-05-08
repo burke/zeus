69512a9a17e454363a7882bb968904e0215d188e
Philipp Fehre <philipp.fehre@googlemail.com>
2013-05-08 10:40:23 +0200

    init sockName to some default value to check against
    get rid of the bool to check if we set yet

--- go/unixsocket/unixsocket.go
+++ go/unixsocket/unixsocket.go
@@ -14,8 +14,7 @@ import (
 // http://code.google.com/p/rsc/source/browse/fuse/mount_linux.go
 // https://github.com/hanwen/go-fuse/blob/master/fuse/mount.go
 // http://code.google.com/p/go/source/browse/src/pkg/syscall/syscall_bsd.go?spec=svn982df2b2cb4b6001e8b60f9e8a000751e9a42198&name=982df2b2cb4b&r=982df2b2cb4b6001e8b60f9e8a000751e9a42198
-var sockName string
-var sockSet bool
+var sockName string = "NOTSET"
 
 type Usock struct {
 	Conn           *net.UnixConn
@@ -26,7 +25,7 @@ type Usock struct {
 }
 
 func ZeusSockName() string {
-  if sockSet {
+  if sockName != "NOTSET" {
     return sockName
   }
 
@@ -34,7 +33,6 @@ func ZeusSockName() string {
   if sockName == "" {
     sockName = ".zeus.sock"
   }
-  sockSet = true
   return sockName
 }
 
