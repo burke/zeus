8d26d1e4ed57cb796774f3ad8f75c04f3ba8df70
Chris Jeon <chris0374@gmail.com>
2014-02-04 01:46:58 -0500

    Added alternative command s

--- go/cmd/zeus/zeus.go
+++ go/cmd/zeus/zeus.go
@@ -61,7 +61,7 @@ func main() {
 		commandSpecificHelp(Args)
 	} else if Args[0] == "version" {
 		printVersion()
-	} else if Args[0] == "start" {
+	} else if Args[0] == "start" || Args[0] == "s" {
 		zeusmaster.Run()
 	} else if Args[0] == "init" {
 		zeusInit()
@@ -154,7 +154,7 @@ func commandNotFound(command string) {
 }
 
 func commandSpecificHelp(args []string) {
-	if args[1] == "start" {
+	if args[1] == "start" || Args[1] == "s" {
 		execManPage("zeus-start")
 	} else if args[1] == "init" {
 		execManPage("zeus-init")
