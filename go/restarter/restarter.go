0da26c9a50b29aa873a979742e6184b212b614a6
Burke Libbey <burke@libbey.me>
2014-02-27 23:17:19 -0500

    Fixed bug just introduced in restarter

--- go/restarter/restarter.go
+++ go/restarter/restarter.go
@@ -10,38 +10,52 @@ var FileChangeWindow = 300 * time.Millisecond
 
 func Start(tree *processtree.ProcessTree, filesChanged chan string, done chan bool) (quit chan bool) {
 	quit = make(chan bool)
-	go start(tree, filesChanged, done, quit)
+	rst := &restarter{tree, filesChanged, done, quit}
+	go rst.start()
 	return quit
+}
+
+type restarter struct {
+	tree         *processtree.ProcessTree
+	filesChanged chan string
+	done, quit   chan bool
 }
 
 // Collect file changes that happen FileChangeWindow ms from each
 // other, and restart all nodes in the process tree that match
 // features of the changed files.
-func start(tree *processtree.ProcessTree, filesChanged chan string, done, quit chan bool) {
+func (r *restarter) start() {
 	for {
 		select {
-		case <-quit:
-			done <- true
+		case <-r.quit:
+			r.done <- true
 			return
-		case file := <-filesChanged:
+		case file := <-r.filesChanged:
 			changed := make(map[string]bool)
 			changed[file] = true
 
 			slog.Trace("Restarter got the first file of potentially many")
-			deadline := time.After(FileChangeWindow)
-			for {
-				select {
-				case <-quit:
-					done <- true
-					return
-				case file := <-filesChanged:
-					changed[file] = true
-				case <-deadline:
-					break
-				}
+
+			if r.gatherFiles(changed, time.After(FileChangeWindow)) {
+				return
 			}
+
 			slog.Trace("Restarter has gathered %d changed files", len(changed))
-			go tree.RestartNodesWithFeatures(changed)
+			go r.tree.RestartNodesWithFeatures(changed)
+		}
+	}
+}
+
+func (r *restarter) gatherFiles(changed map[string]bool, deadline <-chan time.Time) (quitNow bool) {
+	for {
+		select {
+		case <-r.quit:
+			r.done <- true
+			return true
+		case file := <-r.filesChanged:
+			changed[file] = true
+		case <-deadline:
+			return false
 		}
 	}
 }
