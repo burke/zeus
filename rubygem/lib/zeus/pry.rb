ddccf1458784f0c2adf89e662bd6603dd34f23ba
Noah Botimer <botimer@umich.edu>
2018-03-07 19:01:35 -0500

    Don't require pry; only patch it if it is loaded

--- rubygem/lib/zeus/pry.rb
+++ rubygem/lib/zeus/pry.rb
@@ -1,13 +1,9 @@
-begin
-  require "pry"
-
+# Only patch Pry if it is loaded by the app.
+if defined?(Pry::Pager)
   class Pry::Pager
     def best_available
       # paging does not work in zeus so disable it
       NullPager.new(_pry_.output)
     end
   end
-
-rescue LoadError => e
-  # pry is not available, so no need to patch it
 end
