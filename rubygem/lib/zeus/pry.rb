f3fcd67f116d97315ac7ca729c99207a7c943a50
rg <1aab@protonmail.com>
2020-09-11 13:34:55 +0200

    Fix compatibility with Pry 0.13+
    This change allows Zeus to work with both Pry v0.13+ as well as
    versions prior to that.
    
    Fixes #674

--- rubygem/lib/zeus/pry.rb
+++ rubygem/lib/zeus/pry.rb
@@ -3,8 +3,11 @@ begin
 
   class Pry::Pager
     def best_available
+      # Versions of Pry prior to 0.13 define `Pry::Pager#_pry_`
+      # while versions after that define `Pry::Pager#pry_instance`
+      pry = respond_to?(:pry_instance) ? pry_instance : _pry_
       # paging does not work in zeus so disable it
-      NullPager.new(_pry_.output)
+      NullPager.new(pry.output)
     end
   end
 
